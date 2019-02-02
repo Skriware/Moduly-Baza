fe78fea3f66ac072700ccbe84efff0db1d97f017
adzierzanowski <a.dzierzanowski1@gmail.com>
2019-02-02 14:16:55 +0100

    SSD1306 stub

---
+++ stm32/SSD1306/SSD1306.c
@@ -0,0 +1,133 @@
+#include "SSD1306.h"
+
+SSD1306::SSD1306(SPI_HandleTypeDef *hspi,
+  GPIO_TypeDef *rstPort, uint16_t rstPin,
+  GPIO_TypeDef *dcPort, uint16_t dcPin,
+  uint8_t width, uint8_t height)
+  : hspi(hspi),
+    rstPort(rstPort), rstPin(rstPin),
+    dcPort(dcPort), dcPin(dcPin),
+    width(width), height(height)
+{
+  this->buffer.resize(this->width * (this->height / 8));
+}
+
+void SSD1306::init()
+{
+  HAL_GPIO_WritePin(this->dcPort, this->dcPin, GPIO_PIN_RESET);
+
+  HAL_GPIO_WritePin(this->rstPort, this->rstPin, GPIO_PIN_SET);
+  HAL_Delay(1);
+  HAL_GPIO_WritePin(this->rstPort, this->rstPin, GPIO_PIN_RESET);
+  HAL_Delay(10);
+  HAL_GPIO_WritePin(this->rstPort, this->rstPin, GPIO_PIN_SET);
+
+  uint8_t mplex = (uint8_t) this->height - 1;
+
+  uint8_t initCmds[] = {
+    SSD1306::DISPLAYOFF,
+    SSD1306::SETDISPLAYCLOCKDIV,
+    0x80,
+    SSD1306::SETMULTIPLEX,
+    mplex,
+    SSD1306::SETDISPLAYOFFSET,
+    0x0,
+    SSD1306::SETSTARTLINE,
+    SSD1306::CHARGEPUMP,
+    0x14,
+    SSD1306::MEMORYMODE,
+    0x0,
+    SSD1306::SEGREMAP | 0x1,
+    SSD1306::COMSCANDEC,
+    SSD1306::SETCOMPINS,
+    0x12,
+    SSD1306::SETCONTRAST,
+    0xcf,
+    SSD1306::SETPRECHARGE,
+    0xf1,
+    SSD1306::SETVCOMDETECT,
+    0x40,
+    SSD1306::DISPLAYALLON_RESUME,
+    SSD1306::NORMALDISPLAY,
+    SSD1306::DEACTIVATE_SCROLL,
+    SSD1306::DISPLAYON
+  };
+
+  HAL_SPI_Transmit_IT(this->hspi, initCmds, sizeof(initCmds));
+}
+
+void SSD1306::home()
+{
+  uint8_t cmdHome[] = { 0x0, 0x10 };
+  this->sendCmd(cmdHome, 2);
+}
+
+void SSD1306::clear()
+{
+  std::fill(buffer.begin(), buffer.end(), 0);
+}
+
+void SSD1306::sendCmd(uint8_t *cmd, int cmdLen)
+{
+  HAL_GPIO_WritePin(this->dcPort, this->dcPin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit_IT(this->hspi, cmd, cmdLen);
+}
+
+void SSD1306::sendData(uint8_t *data, int dataLen)
+{
+  HAL_GPIO_WritePin(this->dcPort, this->dcPin, GPIO_PIN_SET);
+  HAL_SPI_Transmit_IT(this->hspi, data, dataLen);
+}
+
+void SSD1306::update()
+{
+  this->home();
+
+  // It is guaranteed that std::vector stores the internal array contiguously
+  this->sendData((uint8_t *) &this->buffer[0], this->buffer.size());
+}
+
+int SSD1306::bufferOffset(int x, int y)
+{
+  return y/8 * this->width + x;
+}
+
+int SSD1306::blockOffset(int y)
+{
+  return y - (int)(y/8) * 8;
+}
+
+void SSD1306::putPixel(int x, int y, const uint8_t state)
+{
+  int i = this->bufferOffset(x, y);
+
+  switch (state)
+  {
+    case SSD1306::PIXEL_RESET:
+    case SSD1306::PIXEL_SET:
+      this->buffer[i] |= state << this->blockOffset(y);
+      break;
+    default:
+      break;
+  }
+}
+
+// Right now only images placed at y: y % 8 == 0 are supported
+void SSD1306::bitmap(int x, int y, uint8_t *bmp, int w, int h)
+{
+  int bmpPos = 0;
+  int dy = y;
+
+  while (dy < h)
+  {
+    int offset = this->bufferOffset(x, y+dy);
+    for (int i = offset; i < offset + w; i++)
+    {
+      this->buffer[i] = bmp[bmpPos];
+      bmpPos++;
+      this->update();
+    }
+
+    dy += 8;
+  }
+}
---
+++ stm32/SSD1306/SSD1306.h
@@ -0,0 +1,76 @@
+#include <inttypes.h>
+#include <vector>
+#include "spi.h"
+
+class SSD1306
+{
+  public:
+    static const uint8_t MEMORYMODE = 0x20;
+    static const uint8_t COLUMNADDR = 0x21;
+    static const uint8_t PAGEADDR = 0x22;
+    static const uint8_t SETCONTRAST = 0x81;
+    static const uint8_t CHARGEPUMP = 0x8D;
+    static const uint8_t SEGREMAP = 0xA0;
+    static const uint8_t DISPLAYALLON_RESUME = 0xA4;
+    static const uint8_t DISPLAYALLON = 0xA5;
+    static const uint8_t NORMALDISPLAY = 0xA6;
+    static const uint8_t INVERTDISPLAY = 0xA7;
+    static const uint8_t SETMULTIPLEX = 0xA8;
+    static const uint8_t DISPLAYOFF = 0xAE;
+    static const uint8_t DISPLAYON = 0xAF;
+    static const uint8_t COMSCANINC = 0xC0;
+    static const uint8_t COMSCANDEC = 0xC8;
+    static const uint8_t SETDISPLAYOFFSET = 0xD3;
+    static const uint8_t SETDISPLAYCLOCKDIV = 0xD5;
+    static const uint8_t SETPRECHARGE = 0xD9;
+    static const uint8_t SETCOMPINS = 0xDA;
+    static const uint8_t SETVCOMDETECT = 0xDB;
+
+    static const uint8_t SETLOWCOLUMN = 0x00;
+    static const uint8_t SETHIGHCOLUMN = 0x10;
+    static const uint8_t SETSTARTLINE = 0x40;
+
+    static const uint8_t EXTERNALVCC = 0x01;
+    static const uint8_t SWITCHCAPVCC = 0x02;
+
+    static const uint8_t RIGHT_HORIZONTAL_SCROLL = 0x26;
+    static const uint8_t LEFT_HORIZONTAL_SCROLL = 0x27;
+    static const uint8_t VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL = 0x29;
+    static const uint8_t VERTICAL_AND_LEFT_HORIZONTAL_SCROLL = 0x2A;
+    static const uint8_t DEACTIVATE_SCROLL = 0x2E;
+    static const uint8_t ACTIVATE_SCROLL = 0x2F;
+    static const uint8_t SET_VERTICAL_SCROLL_AREA = 0xA3;
+
+    static const uint8_t PIXEL_RESET = 0;
+    static const uint8_t PIXEL_SET = 1;
+    static const uint8_t PIXEL_TOGGLE = 2;
+
+  private:
+    SPI_HandleTypeDef *hspi;
+    GPIO_TypeDef *rstPort;
+    uint16_t rstPin;
+    GPIO_TypeDef *dcPort;
+    uint16_t dcPin;
+
+  public:
+    std::vector<uint8_t> buffer;
+    uint8_t width;
+    uint8_t height;
+
+
+    SSD1306(SPI_HandleTypeDef *hspi,
+      GPIO_TypeDef *rstPort, uint16_t rstPin,
+      GPIO_TypeDef *dcPort, uint16_t dcPin,
+      uint8_t width, uint8_t height);
+
+    void home(void);
+    void init(void);
+    void clear(void);
+    void sendCmd(uint8_t *cmd, int cmdLen);
+    void sendData(uint8_t *data, int dataLen);
+    void update(void);
+    void putPixel(int x, int y, const uint8_t state);
+    void bitmap(int x, int y, uint8_t *bmp, int w, int h);
+    int bufferOffset(int x, int y);
+    int blockOffset(int y);
+};
